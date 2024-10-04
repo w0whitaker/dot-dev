39cc122489b25dc897737f63e77262b12be9a935
William Whitaker <william@williamwhitaker.dev>
2024-10-04 15:00:27 -0400

    basic setup

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,12 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "william whitaker dot dev",
+	url: "https://williamwhitaker.dev/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description:
+		"Projects, notes, and thoughts about and relating to web development.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		name: "William Whitaker",
+		email: "william@williamwhitaker.dev",
+		url: "https://williamwhitaker.dev/about/",
+	},
+};
---
+++ content/blog/eleven-tea.md
@@ -0,0 +1,17 @@
+---
+title: "bad eleventy puns"
+---
+
+# eleven teas
+
+1. darjeeling
+2. black
+3. green
+4. mint
+5. iced
+6. chamomile
+7. english breakfast
+8. earl grey
+9. chai
+10. rooibos
+11. texas
